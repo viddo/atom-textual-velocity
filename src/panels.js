0e6019a3938b202db670a6c10955b07fd6d4f4b8
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2015-10-06 18:42:39 +0300

    :art: Renamings to clarify intents

--- src/panels.js
+++ src/panels.js
@@ -1,93 +1,21 @@
 'use babel';
 
 import {Disposable, CompositeDisposable} from 'atom';
-import R from 'ramda';
-import Path from 'path';
-import React from 'react-for-atom';
-import Bacon from 'baconjs';
-import Panel from './react-components/panel';
-import atoms from './atom-streams';
-import columns from './columns';
+import NotationalPanel from './notational-panel';
 
-// Wrap the panel component, bridges between Atom context and the React panel component
+// Manages the state of the panels
 class Panels {
 
   constructor(projects) {
-    let columnsProp = Bacon.sequentially(0, [columns]).toProperty([]);
-    let dblCancelStream = this._doubleTapStream(atoms.cancelCommand());
-
-    this._panelElement = document.createElement('div');
-    this._atomPanel = atom.workspace.addTopPanel({item: this._panelElement});
-    let reactPanel = React.render(
-      <Panel columnsProp={columnsProp}
-        showStream={dblCancelStream}
-        bodyHeightStream={atoms.fromConfig('atom-notational.bodyHeight')}
-        searchBus={projects.searchBus}
-        matchedItemsProp={projects.matchedItemsProp}
-      />, this._panelElement);
-
-    let dblResetStream = this._doubleTapStream(reactPanel.resetStream);
-
-    this._addSideEffects([
-      projects,
-
-      reactPanel.bodyHeightProp.debounce(500).onValue(newHeight =>
-        atom.config.set('atom-notational.bodyHeight', newHeight)
-      ),
-
-      reactPanel.guaranteedSelectedItemProp.onValue(selectedItem => {
-        // TODO: for now only preview files if the preview tabs are enabled
-        if (atom.config.get('tabs.usePreviewTabs')) {
-          // don't activate pane to keep focus on the search input/top panel
-          atom.workspace.open(Path.join(selectedItem.projectPath, selectedItem.relPath), {activatePane: false});
-        }
-      }),
-
-      reactPanel.guaranteedSelectedItemProp.sampledBy(reactPanel.openStream).onValue(selectedItem => {
-        atom.workspace.open(Path.join(selectedItem.projectPath, selectedItem.relPath));
-      }),
-
-      dblCancelStream.onValue(() => this._atomPanel.show()),
-
-      dblResetStream.onValue(() => {
-        this._atomPanel.hide();
-        let activePane = atom.workspace.getActivePane();
-        if (activePane) {
-          activePane.activate();
-        }
-      }),
-    ]);
+    this.disposables = new CompositeDisposable();
+    this.panel = new NotationalPanel(projects);
+    this.disposables.add(this.panel);
   }
 
   dispose() {
-    React.unmountComponentAtNode(this._panelElement);
-    this._panelElement = null;
-    this._atomPanel.destroy();
-    this._atomPanel = null;
-
     this.disposables.dispose();
     this.disposables = null;
-  }
-
-  _addSideEffects(list) {
-    this.disposables = new CompositeDisposable();
-    for (sideEffect of list) {
-      this.disposables.add(this._makeDisposable(sideEffect));
-    }
-  }
-
-  _makeDisposable(o) {
-    if (Disposable.isDisposable(o)) return o;
-    if (typeof o === 'function') {
-      return new Disposable(o);
-    } else {
-      throw new Error('must be a function');
-    }
-  }
-
-  // filter a given stream to only trigger if tow event are triggered within 300ms (e.g. double-ESC)
-  _doubleTapStream(stream) {
-    return stream.bufferWithTimeOrCount(300, 2).filter(R.propEq('length', 2));
+    this.panel = null;
   }
 };
 
