e4b35ed9b59419ef38fd058f3b911664eea817e4
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2016-01-09 21:26:25 +0100

    :bug: :art: Maintain highlight on pagination
    Also DRY code, by consolidating the join pattern callback used to
    create the results object

---
+++ spec/get-result-spec.js
@@ -0,0 +1,46 @@
+'use babel'
+
+import getResult from '../lib/get-result'
+
+describe('getResult', function () {
+  let results
+  const items = [
+    {path: '/foo'},
+    {path: '/bar'},
+    {path: '/baz'},
+    {path: '/pax'}
+  ]
+
+  describe('when there are some results', function () {
+    beforeEach(function () {
+      const query = {
+        searchStr: '',
+        paginationOffset: 1,
+        paginationSize: 2
+      }
+      const queryResults = {
+        total: 2,
+        tokens: [],
+        items: [
+          {id: 0},
+          {id: 1},
+          {id: 2},
+          {id: 3}
+        ]
+      }
+      results = getResult(query, queryResults, items)
+    })
+
+    it('results has correct total', function () {
+      expect(results.total).toEqual(2)
+    })
+
+    it('results has expected items', function () {
+      expect(results.items.length).toEqual(2)
+    })
+
+    it('results items have hrtime when item was created', function () {
+      expect(results.items[0].hrtime).toBeDefined()
+    })
+  })
+})
