8c0765ae0d0a2a042f5f5f828339fddcf9c09a80
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2016-01-09 18:22:13 +0100

    :bug: Fix prev/next key navigation

---
+++ spec/indices-spec.js
@@ -0,0 +1,55 @@
+'use babel'
+
+import { prev, next } from '../lib/indices'
+
+describe('indices', function () {
+  describe('.prev', function () {
+    it('returns the last index when there is no current index', function () {
+      expect(prev(123)).toEqual(122)
+      expect(prev(123, undefined)).toEqual(122)
+      expect(prev(123, null)).toEqual(122)
+    })
+
+    it('stops at first index', function () {
+      expect(prev(123, 0)).toEqual(0)
+    })
+
+    it('returns the prev index while within the total range', function () {
+      expect(prev(123, 1)).toEqual(0)
+      expect(prev(123, 51)).toEqual(50)
+      expect(prev(123, 122)).toEqual(121)
+    })
+
+    it('returns closest within-range index when out of range', function () {
+      expect(prev(123, -9000)).toEqual(0)
+      expect(prev(123, -1)).toEqual(0)
+      expect(prev(123, 123)).toEqual(122)
+      expect(prev(123, 9000)).toEqual(122)
+    })
+  })
+
+  describe('.next', function () {
+    it('returns the first index when there is no current index', function () {
+      expect(next(123)).toEqual(0)
+      expect(next(123, undefined)).toEqual(0)
+      expect(next(123, null)).toEqual(0)
+    })
+
+    it('stops at the last index', function () {
+      expect(next(123, 122)).toEqual(122)
+    })
+
+    it('returns the next index while within the total range', function () {
+      expect(next(123, 0)).toEqual(1)
+      expect(next(123, 51)).toEqual(52)
+      expect(next(123, 121)).toEqual(122)
+    })
+
+    it('stops at the closest within-range index when out of range', function () {
+      expect(next(123, -9000)).toEqual(0)
+      expect(next(123, -1)).toEqual(0)
+      expect(next(123, 123)).toEqual(122)
+      expect(next(123, 9000)).toEqual(122)
+    })
+  })
+})
