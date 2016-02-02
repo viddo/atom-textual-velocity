2df679cb9fe8d92c43d7920f0fad39fb56bb616f
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2016-02-02 23:23:38 +0100

    :tada: Make file exts less prominent
    Because it’s not as important as the actual name of the file.
    
    Also upgraded the react-for-atom dependency finally, to have access to
    TestUtils and fix some errors/warnings thrown in dev console.

---
+++ spec/react/cells/summary-title-spec.js
@@ -0,0 +1,39 @@
+'use babel'
+
+import { React, TestUtils } from 'react-for-atom'
+import SummaryTitle from '../../../lib/react/cells/summary-title'
+
+describe('react/cells/summary-title', function () {
+  let renderer, r
+
+  beforeEach(function () {
+    renderer = TestUtils.createRenderer()
+  })
+
+  describe('when there is no regexp', function () {
+    it('renders name of normal file', function () {
+      renderer.render(<SummaryTitle name='filename' ext='txt' regexp={null} />)
+      r = renderer.getRenderOutput()
+      expect(r.props.children[0]).toEqual('filename')
+      expect(r.props.children[1]).toEqual(<span className='text-subtle'>.txt</span>)
+    })
+
+    it('renders only filename when there is no file ext', function () {
+      renderer.render(<SummaryTitle name='filename' regexp={null} />)
+      r = renderer.getRenderOutput()
+      expect(r.props.children[0]).toEqual('filename')
+      expect(r.props.children[1].props.children).toEqual('')
+    })
+  })
+
+  describe('when there is a regexp', function () {
+    it('highlights the matching part of the name', function () {
+      renderer.render(<SummaryTitle name='filename' ext='txt' regexp={/enam/} />)
+      r = renderer.getRenderOutput()
+      expect(r.props.children[0]).toEqual('fil')
+      expect(r.props.children[1]).toEqual(<span className='text-highlight'>enam</span>)
+      expect(r.props.children[2]).toEqual('e')
+      expect(r.props.children[3]).toEqual(<span className='text-subtle'>.txt</span>)
+    })
+  })
+})
