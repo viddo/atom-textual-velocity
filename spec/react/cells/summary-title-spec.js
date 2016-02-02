d39732e9fa1cca09f8ee997f94dd945910f601a9
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2016-02-03 00:58:23 +0100

    :bug: Remove double dots between file name and ext

--- spec/react/cells/summary-title-spec.js
+++ spec/react/cells/summary-title-spec.js
@@ -12,7 +12,7 @@ describe('react/cells/summary-title', function () {
 
   describe('when there is no regexp', function () {
     it('renders name of normal file', function () {
-      renderer.render(<SummaryTitle name='filename' ext='txt' regexp={null} />)
+      renderer.render(<SummaryTitle name='filename' ext='.txt' regexp={null} />)
       r = renderer.getRenderOutput()
       expect(r.props.children[0]).toEqual('filename')
       expect(r.props.children[1]).toEqual(<span className='text-subtle'>.txt</span>)
@@ -28,12 +28,12 @@ describe('react/cells/summary-title', function () {
 
   describe('when there is a regexp', function () {
     it('highlights the matching part of the name', function () {
-      renderer.render(<SummaryTitle name='filename' ext='txt' regexp={/enam/} />)
+      renderer.render(<SummaryTitle name='filename' ext='.md' regexp={/enam/} />)
       r = renderer.getRenderOutput()
       expect(r.props.children[0]).toEqual('fil')
       expect(r.props.children[1]).toEqual(<span className='text-highlight'>enam</span>)
       expect(r.props.children[2]).toEqual('e')
-      expect(r.props.children[3]).toEqual(<span className='text-subtle'>.txt</span>)
+      expect(r.props.children[3]).toEqual(<span className='text-subtle'>.md</span>)
     })
   })
 })
