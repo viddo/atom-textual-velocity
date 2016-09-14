f0b2aa7a633e8d8f95cccbb95eacf3663f90c84a
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2016-09-14 16:27:41 +0200

    :truck: Extract file icons to a column of its own
    Instead of having it within the summary column

---
+++ spec/columns/file-icon-column-spec.js
@@ -0,0 +1,40 @@
+'use babel'
+
+import NotesFile from '../../lib/notes-file'
+import FileIconColumn from '../../lib/columns/file-icon-column'
+
+describe('columns/file-icon-column', function () {
+  let column: ColumnType
+
+  beforeEach(function () {
+    column = new FileIconColumn({sortField: 'ext'})
+  })
+
+  describe('.sortField', function () {
+    it('should return given sort field value', function () {
+      expect(column.sortField).toEqual('ext')
+    })
+  })
+
+  describe('.cellContent', function () {
+    let file, cellContent
+
+    beforeEach(function () {
+      file = new NotesFile('markdown.md', relPath => `/notes/${relPath}`, {
+        content: '# testing markdown\nshould be **Zzz** by now tho'
+      })
+      cellContent = column.cellContent(file)
+    })
+
+    it('should return a kind of AST from which a DOM can be created', function () {
+      expect(cellContent).toEqual(jasmine.any(Object), 'title')
+      expect(cellContent.attrs).toEqual({
+        title: 'File extension: .md',
+        className: 'icon icon-file-text',
+        'data-name': 'markdown.md',
+        'data-path': '/notes/markdown.md'
+      })
+      expect(cellContent.content).toEqual(jasmine.any(String))
+    })
+  })
+})
