a963217c5bd238632ed6c3745c82444491c46115
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2016-09-22 10:43:38 +0200

    :art: Add keymap for rename-note
    Refactored internal for the feature to be consolidated into one place

---
+++ decls/textual-velocity/public-service-type.js
@@ -0,0 +1,7 @@
+declare type PublicServiceType = {
+  registerColumns (...items: Array<ColumnType>): void,
+  registerFields (...items: Array<FieldType>): void,
+  registerFileReaders (...items: Array<FileReaderType>): void,
+  registerFileWriters (...items: Array<FileWriterType>): void,
+  editCell (editCellName: string): void
+}
