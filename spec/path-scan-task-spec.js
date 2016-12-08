0d0cdb12988b80b622aea6facca44ad591027285
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2016-12-07 17:15:18 -0800

    :gift: Move initial scan to an redux-observable epic

--- spec/path-scan-task-spec.js
+++ spec/path-scan-task-spec.js
@@ -8,7 +8,7 @@ import temp from 'temp'
 temp.track()
 
 describe('path-scan-task', () => {
-  let task, dir
+  let task, dir, addSpy
 
   beforeEach(function () {
     const tempDirPath = temp.mkdirSync('empty-dir')
@@ -21,6 +21,24 @@ describe('path-scan-task', () => {
 
     const taskPath = Path.join(__dirname, '..', 'lib', 'path-scan-task.js')
     task = new Task(taskPath)
+    task.on('task:completed', () => {
+      task.terminate()
+    })
+
+    addSpy = jasmine.createSpy('add')
+    task.on('add', addSpy)
+
+    let done = false
+    task.on('done', () => {
+      done = true
+    })
+    task.start(dir, {
+      ignored: 'node_modules',
+      persistent: true,
+      depth: 0,
+      cwd: dir
+    })
+    waitsFor(() => done)
   })
 
   afterEach(function () {
@@ -28,31 +46,10 @@ describe('path-scan-task', () => {
     task.terminate()
   })
 
-  describe('.start', function () {
-    let addSpy
-
-    beforeEach(function () {
-      addSpy = jasmine.createSpy('add')
-      task.on('add', addSpy)
-
-      let done = false
-      task.on('ready', () => {
-        done = true
-      })
-      task.start(dir, {
-        ignored: 'node_modules',
-        persistent: true,
-        depth: 0,
-        cwd: dir
-      })
-      waitsFor(() => done)
-    })
-
-    it('should yield add events for each file found in dir', function () {
-      expect(addSpy).toHaveBeenCalled()
-      expect(addSpy.calls.length).toEqual(4)
-      expect(addSpy.calls[0].args[0]).toEqual('note-1.txt')
-      expect(addSpy.calls[0].args[1]).toEqual(jasmine.any(Object))
-    })
+  it('should yield add events for each file found in dir', function () {
+    expect(addSpy).toHaveBeenCalled()
+    expect(addSpy.calls.length).toEqual(4)
+    expect(addSpy.calls[0].args[0]).toEqual('note-1.txt')
+    expect(addSpy.calls[0].args[1]).toEqual(jasmine.any(Object))
   })
 })
