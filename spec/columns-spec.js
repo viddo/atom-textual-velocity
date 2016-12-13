68d23a8b2578500e4acefe5bb6eb7c56d7e3b001
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2016-12-13 14:45:36 -0800

    :gift: Update columns schema when adding a new column

--- spec/columns-spec.js
+++ spec/columns-spec.js
@@ -6,6 +6,9 @@ describe('columns', () => {
   let columns
 
   beforeEach(function () {
+    atom.config.setSchema('textual-velocity.sortField', {
+      type: 'string'
+    })
     columns = new Columns()
   })
 
@@ -17,6 +20,35 @@ describe('columns', () => {
         description: 'A test column',
         width: 50,
         cellContent: () => 'some content'
+      })
+    })
+
+    it('should update sortField schema', function () {
+      let schema = atom.config.getSchema('textual-velocity.sortField')
+      expect(schema).toEqual({
+        type: 'string',
+        default: 'test-field',
+        enum: [
+          {value: 'test-field', description: 'test'}
+        ]
+      })
+
+      columns.add({
+        sortField: 'test-field2',
+        title: 'another field',
+        description: 'A second test column',
+        width: 50,
+        cellContent: () => 'some other content'
+      })
+
+      schema = atom.config.getSchema('textual-velocity.sortField')
+      expect(schema).toEqual({
+        type: 'string',
+        default: 'test-field',
+        enum: [
+          {value: 'test-field', description: 'test'},
+          {value: 'test-field2', description: 'another field'}
+        ]
       })
     })
 
