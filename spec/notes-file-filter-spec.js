18aaff049ca3db700d5b4a8ac5a97c503a522d8c
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2017-11-08 22:43:19 -0800

    Simplify notes file filter and usage

--- spec/notes-file-filter-spec.js
+++ spec/notes-file-filter-spec.js
@@ -1,7 +1,6 @@
 "use babel";
 
 import { defaultConfig } from "../lib/config";
-import Path from "path";
 import NotesFileFilter from "../lib/notes-file-filter";
 
 describe("notes-file-filter", () => {
@@ -26,51 +25,31 @@ describe("notes-file-filter", () => {
 
   describe(".isAccepted", function() {
     it("returns true for any text file", function() {
-      expect(notesFileFilter.isAccepted(Path.join(__dirname, "file.txt"))).toBe(
-        true
-      );
-      expect(notesFileFilter.isAccepted(Path.join(__dirname, "file.md"))).toBe(
-        true
-      );
-      expect(notesFileFilter.isAccepted(Path.join(__dirname, "file.js"))).toBe(
-        true
-      );
-      expect(
-        notesFileFilter.isAccepted(Path.join(__dirname, "file.json"))
-      ).toBe(true);
-      expect(
-        notesFileFilter.isAccepted(Path.join(__dirname, "file.bash"))
-      ).toBe(true);
+      expect(notesFileFilter.isAccepted({ filename: "file.txt" })).toBe(true);
+      expect(notesFileFilter.isAccepted({ filename: "file.md" })).toBe(true);
+      expect(notesFileFilter.isAccepted({ filename: "file.js" })).toBe(true);
+      expect(notesFileFilter.isAccepted({ filename: "file.json" })).toBe(true);
+      expect(notesFileFilter.isAccepted({ filename: "file.bash" })).toBe(true);
     });
 
     it("returns false for any non-text file", function() {
-      expect(notesFileFilter.isAccepted(Path.join(__dirname, "file.exe"))).toBe(
-        false
-      );
-      expect(notesFileFilter.isAccepted(Path.join(__dirname, "file.jpg"))).toBe(
-        false
-      );
-      expect(notesFileFilter.isAccepted(Path.join(__dirname, "file.zip"))).toBe(
-        false
-      );
-      expect(notesFileFilter.isAccepted(Path.join(__dirname, "file.pdf"))).toBe(
-        false
-      );
+      expect(notesFileFilter.isAccepted({ filename: "file.exe" })).toBe(false);
+      expect(notesFileFilter.isAccepted({ filename: "file.jpg" })).toBe(false);
+      expect(notesFileFilter.isAccepted({ filename: "file.zip" })).toBe(false);
+      expect(notesFileFilter.isAccepted({ filename: "file.pdf" })).toBe(false);
     });
 
     it("returns false for any excluded file", function() {
-      expect(
-        notesFileFilter.isAccepted(Path.join(__dirname, ".git/index"))
-      ).toBe(false);
-      expect(
-        notesFileFilter.isAccepted(Path.join(__dirname, ".DS_Store"))
-      ).toBe(false);
+      expect(notesFileFilter.isAccepted({ filename: ".git/index" })).toBe(
+        false
+      );
+      expect(notesFileFilter.isAccepted({ filename: ".DS_Store" })).toBe(false);
     });
 
     it("returns false for nv/nvalt settings file", function() {
-      expect(
-        notesFileFilter.isAccepted(Path.join(__dirname, "Notes & Settings"))
-      ).toBe(false);
+      expect(notesFileFilter.isAccepted({ filename: "Notes & Settings" })).toBe(
+        false
+      );
     });
   });
 });
