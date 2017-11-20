20a4fb62f82214b6cebc970877b05308a9b7afda
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2017-11-20 08:23:57 -0800

    Cache notes as metadata file in dir (#112)
    * Store cache in notes dir instead
    
    * Filter out cache file
    
    * Fix info text

--- spec/notes-cache-spec.js
+++ spec/notes-cache-spec.js
@@ -1,49 +1,43 @@
 "use babel";
 
-import { it, fit } from "./_async-spec-helpers"; // eslint-disable-line
+import tempy from "tempy";
 import NotesCache from "../lib/notes-cache";
 
 describe("notes-cache", () => {
   let notes, notesCache;
 
   beforeEach(function() {
-    atom.enablePersistence = true;
-    atom.stateStore.clear();
-    notesCache = new NotesCache("/notes");
+    const tempDirPath = tempy.directory();
+    notesCache = new NotesCache(tempDirPath);
+    spyOn(console, "warn");
   });
 
   afterEach(function() {
-    atom.enablePersistence = false;
     notesCache.dispose();
-    atom.stateStore.clear();
   });
 
-  it("should save and load notes", async function() {
-    notes = await notesCache.load();
+  it("should save and load notes", function() {
+    notes = notesCache.load();
     expect(notes).toEqual({});
 
-    try {
-      await notesCache.save({ "some-file": { foo: "bar" } });
-    } catch (err) {
-      this.fail("save should work");
-    }
-    notes = await notesCache.load();
+    notesCache.save({ "some-file": { foo: "bar" } });
+    notes = notesCache.load();
     expect(notes).toEqual({ "some-file": { foo: "bar" } });
   });
 
-  it("should not save notes after clear-notes-cache command is called", async function() {
-    notes = await notesCache.load();
+  it("should not save notes after clear-notes-cache command is called", function() {
+    notes = notesCache.load();
 
     const workspaceView = atom.views.getView(atom.workspace);
     jasmine.attachToDOM(workspaceView);
     atom.commands.dispatch(workspaceView, "textual-velocity:clear-notes-cache");
 
     try {
-      await notesCache.save({ "some-file": { foo: "bar" } });
+      notesCache.save({ "some-file": { foo: "bar" } });
     } catch (err) {
-      this.fail("save should work");
+      throw err;
     }
-    notes = await notesCache.load();
+    notes = notesCache.load();
     expect(notes).toEqual({});
   });
 });
