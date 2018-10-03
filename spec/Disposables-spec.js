d66689017d5779e020269d04611d444d86e1a8fb
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2018-10-02 23:54:42 -0700

    Make Disposables compatible with new atom beta

---
+++ spec/Disposables-spec.js
@@ -0,0 +1,30 @@
+/* @flow */
+
+import Disposables from "../lib/Disposables";
+
+const newFakeDisposable = () => {
+  const spy = jasmine.createSpy("dispose");
+  return {
+    dispose: spy,
+    disposed: () => true
+  };
+};
+
+describe("Disposables", () => {
+  let disposables;
+
+  beforeEach(() => {
+    disposables = new Disposables();
+  });
+
+  describe(".add", () => {
+    it("accepts multiple values", () => {
+      const fake1 = newFakeDisposable();
+      const fake2 = newFakeDisposable();
+      disposables.add(fake1, fake2);
+      disposables.dispose();
+      expect(fake1.dispose).toHaveBeenCalled();
+      expect(fake2.dispose).toHaveBeenCalled();
+    });
+  });
+});
