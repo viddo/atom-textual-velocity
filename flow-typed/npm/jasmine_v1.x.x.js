34dcdb4715380389d4cb47390d4918facce3031b
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2018-11-17 17:34:28 -0800

    Downgrade flow to v0.84 (#347)
    * Fail lint:flow if there are errors
    
    * Downgrade flow version
    
    Due to problems in upstream type defs
    
    * Update flowtype defs
    
    * Minor flow fixes

--- flow-typed/npm/jasmine_v1.x.x.js
+++ flow-typed/npm/jasmine_v1.x.x.js
@@ -1,15 +1,15 @@
-declare function advanceClock (ms: number): void
-declare function afterEach (fn: JasmineFn): void
-declare function beforeEach (fn: JasmineFn): void
-declare function describe (desc: string, fn: JasmineFn): void
-declare function xdescribe (desc: string, fn: JasmineFn): void
-declare function expect (actual: any): Object
-declare function it (desc: string, fn: JasmineFn): void
-declare function runs (fn: Function): void
-declare function spyOn (actual: Object, method: string): Object
-declare function waitsFor (fn: Function): void
-declare function waitsForPromise (fn: Function): void
+declare function advanceClock(ms: number): void;
+declare function afterEach(fn: typeof JasmineFn): void;
+declare function beforeEach(fn: typeof JasmineFn): void;
+declare function describe(desc: string, fn: typeof JasmineFn): void;
+declare function xdescribe(desc: string, fn: typeof JasmineFn): void;
+declare function expect(actual: any): Object;
+declare function it(desc: string, fn: typeof JasmineFn): void;
+declare function runs(fn: () => void): void;
+declare function spyOn(actual: Object, method: string): Object;
+declare function waitsFor(fn: () => boolean): void;
+declare function waitsForPromise(fn: () => Promise<any>): void;
 
-declare function JasmineFn (done?: Function): void
+declare function JasmineFn(done?: () => void): void;
 
-declare var jasmine: Object
+declare var jasmine: Object;
